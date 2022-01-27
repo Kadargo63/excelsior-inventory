ad487fe6545774fe2f6e7b72ec9e37b58aa50361
Maria Sori <mariasori.school@gmail.com>
2022-01-27 18:20:39 -0500

    updates to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,15 +5,96 @@ const { signToken } = require('../utils/auth');
 //create resolvers
 const resolvers = {
     Query: {
-        //Item.find() - get all items
-        //Item.findOne - get single item
+        //categories
+        categories: async () => {
+            return await Category.find();
+        },
+        //products
+        products: async (parent, { category, name }) => {
+            const params = {};
+      
+            if (category) {
+              params.category = category;
+            }
+      
+            if (name) {
+              params.name = {
+                $regex: name
+              };
+            }
+      
+            return await Product.find(params).populate('category');
+        },
+        //product
+        product: async (parent, { _id }) => {
+            return await Product.findById(_id).populate('category');
+        },
+        //user
+        user: async (parent, args, context) => {
+            if (context.user) {
+              const user = await User.findById(context.user._id).populate({
+                path: 'orders.products',
+                populate: 'category'
+              });
+      
+              user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+      
+              return user;
+            }
+      
+            throw new AuthenticationError('Not logged in');
+        },
+        //inventory
     },
 
     Mutation: {
-        //add user?
+        //add user
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+      
+            return { token, user };
+        },
+        //addInventory
+        addInventory: async (parent, args) => {
+            
+
+
+        },
+        //updateUser
+        updateUser: async (parent, args, context) => {
+            if (context.user) {
+              return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+            }
+      
+            throw new AuthenticationError('Not logged in');
+        },
+        //updateProduct
+        updateProduct: async (parent, { _id, quantity }) => {
+            const decrement = Math.abs(quantity) * -1;
+      
+            return await Product.findByIdAndUpdate(_id, { $inc: { quantity: decrement } }, { new: true });
+        },
+        //updateInventory
+        
         //login
-        //add item
-        //update single item
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+        }
     }
 }
 
