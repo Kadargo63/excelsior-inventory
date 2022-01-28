6bc0dea5b071540d60dc92f13703a2509eba06fc
Jamie Williams <jamwil1226@yahoo.com>
2022-01-28 11:00:16 -0500

    nav set up

---
+++ client/src/components/ItemList/index.js
@@ -0,0 +1,70 @@
+import React, { useEffect } from 'react';
+// import ProductItem from '../ProductItem';
+import { useStoreContext } from '../../utils/GlobalState';
+import { UPDATE_ITEMS } from '../../utils/actions';
+import { useQuery } from '@apollo/client';
+import { QUERY_PRODUCTS } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
+import spinner from '../../assets/spinner.gif';
+
+function ItemList() {
+  const [state, dispatch] = useStoreContext();
+
+  const { currentCategory } = state;
+
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+  useEffect(() => {
+    if (data) {
+      dispatch({
+        type: UPDATE_ITEMS,
+        items: data.items,
+      });
+      data.items.forEach((items) => {
+        idbPromise('items', 'put', items);
+      });
+    } else if (!loading) {
+      idbPromise('items', 'get').then((items) => {
+        dispatch({
+          type: UPDATE_ITEMS,
+          items: items,
+        });
+      });
+    }
+  }, [data, loading, dispatch]);
+
+  function filterProducts() {
+    if (!currentCategory) {
+      return state.products;
+    }
+
+    return state.products.filter(
+      (product) => product.category._id === currentCategory
+    );
+  }
+
+  return (
+    <div className="my-2">
+      <h2>Items Available:</h2>
+      {state.products.length ? (
+        <div className="flex-row">
+          {filterProducts().map((product) => (
+            <ProductItem
+              key={product._id}
+              _id={product._id}
+              image={product.image}
+              name={product.name}
+              size={product.size}
+              quantity={product.quantity}
+            />
+          ))}
+        </div>
+      ) : (
+        <h3>You haven't added any products yet!</h3>
+      )}
+      {loading ? <img src={spinner} alt="loading" /> : null}
+    </div>
+  );
+}
+
+export default ProductList;
