f6e93920c8b9a6e1c441132a6856fc29a4c66894
Jamie Williams <jamwil1226@yahoo.com>
2022-01-30 11:32:32 -0500

    code refactor

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,30 +6,25 @@ const typeDefs = gql`
     name: String
   }
 
-  type Item {
+  type Product {
     _id: ID
     name: String
     image: String
     quantity: Int
     size: String
     category: Category
-    location: ID
   }
 
-  type Onhands {
-      items: [Item]
-      location: ID
-      quantity: Int
-  }
-
-  type Location {
+  type Order {
     _id: ID
+    purchaseDate: String
     products: [Product]
   }
 
   type User {
     _id: ID
-    name: String
+    firstName: String
+    lastName: String
     email: String
     orders: [Order]
   }
@@ -45,19 +40,9 @@ const typeDefs = gql`
 
   type Query {
     categories: [Category]
-    items(category: ID, name: String): [Item]
-    item(_id: ID!): Item
-    onHands(location: ID, item: ID): OnHands
+    products(category: ID, name: String): [Product]
+    product(_id: ID!): Product
     user: User
-<<<<<<< HEAD
-  }
-
-  type Mutation {
-    addUser(name: String!, email: String!, password: String!): Auth
-    addItem(products: [ID]!): Item
-    addLocation(products: [ID]!): Location
-    updateOnHands(products: [ID]!): Item
-=======
     order(_id: ID!): Order
     checkout(products: [ID]!): Checkout
   }
@@ -67,7 +52,6 @@ const typeDefs = gql`
     addOrder(products: [ID]!): Order
     updateUser(firstName: String, lastName: String, email: String, password: String): User
     updateProduct(_id: ID!, quantity: Int!): Product
->>>>>>> feature/ri-typedefs
     login(email: String!, password: String!): Auth
   }
 `;
